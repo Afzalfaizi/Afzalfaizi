685303f14dfa649875df3309f2eb4d039660a0e1
afzalfaizi <afzaalm993@gmail.com>
2023-08-25 13:06:57 +0500

    while loop added

---
+++ class6.js
@@ -0,0 +1,15 @@
+"use strict";
+var index = 1;
+while (index < 10) {
+    console.log(index);
+    index++;
+}
+console.log("console");
+// do while loop parctice
+for (var index = 10; index >= 5; index--) {
+}
+let i = 10;
+do {
+    console.log("Naveed");
+    i++;
+} while (i < 10);
